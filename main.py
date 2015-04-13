d094d6ee4c355e864dd07cd5cf4a50f5a69916b0
Miguel Coronel <m.coronel.seg@gmail.com>
2015-04-13 01:33:41 -0500

    Scarbble Cheater

---
+++ main.py
@@ -0,0 +1,34 @@
+import sys
+import scrabble
+
+def is_valid_word(word, rack):
+    available_letters = rack[:]
+    for letter in word:
+        if letter not in available_letters:
+            return False
+        available_letters.remove(letter)
+    return True
+
+def compute_score(word):
+    score = 0
+    for letter in word:
+        score += scrabble.scores[letter] 
+    return score
+
+if len(sys.argv) < 2:
+    print("Usage: scrabble.py [RACK]")
+    exit(1)
+
+rack = list(sys.argv[1].lower())
+valid_words = []
+
+for word in scrabble.wordlist:
+    if is_valid_word(word, rack):
+        score = compute_score(word)
+        valid_words.append([score, word])
+
+valid_words.sort()
+for play in valid_words:
+    score = play[0]
+    word  = play[1]
+    print(word +  ": " +str(score))
